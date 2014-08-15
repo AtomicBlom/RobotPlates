a2119ef987baae90cbb5dbf4b36915d67a51f312
Steven Blom <atomicblom@binaryvibrance.net>
2014-08-16 01:29:17 +0930

    Start of Compilers

--- src/main/java/net/binaryvibrance/robotplates/block/BlockPlateProgrammer.java
+++ src/main/java/net/binaryvibrance/robotplates/block/BlockPlateProgrammer.java
@@ -1,10 +1,14 @@
 package net.binaryvibrance.robotplates.block;
 
+import net.binaryvibrance.robotplates.programming.PlateCompiler;
 import net.binaryvibrance.robotplates.reference.RenderIds;
 import net.binaryvibrance.robotplates.tileentity.TileEntityPlateProgrammer;
+import net.binaryvibrance.robotplates.utility.LogHelper;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
 
 public class BlockPlateProgrammer extends BaseRobotPlateBlock implements ITileEntityProvider {
@@ -13,6 +17,7 @@ public class BlockPlateProgrammer extends BaseRobotPlateBlock implements ITileEn
 		this.setBlockName("plateProgrammer");
 		this.setBlockTextureName("modelPlateProgrammer");
 		this.setBlockBounds(0.00F, 0.0F, 0.00F, 1.00F, (3f / 16f), 1.00F);
+
 	}
 
 	@Override
@@ -33,5 +38,22 @@ public class BlockPlateProgrammer extends BaseRobotPlateBlock implements ITileEn
 	@Override
 	public TileEntity createNewTileEntity(World world, int metaData) {
 		return new TileEntityPlateProgrammer();
+	}
+
+	@Override
+	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float px, float py, float pz) {
+		if (world.isRemote) {
+			return true;
+		}
+
+		LogHelper.info("Create Blocks");
+
+		TileEntityPlateProgrammer tileEntity = (TileEntityPlateProgrammer) world.getTileEntity(x, y, z);
+		tileEntity.triggerCompile();
+
+
+
+
+		return true;
 	}
 }
--- src/main/java/net/binaryvibrance/robotplates/tileentity/TileEntityPlateProgrammer.java
+++ src/main/java/net/binaryvibrance/robotplates/tileentity/TileEntityPlateProgrammer.java
@@ -1,4 +1,10 @@
 package net.binaryvibrance.robotplates.tileentity;
 
+import net.binaryvibrance.robotplates.programming.PlateCompiler;
+
 public class TileEntityPlateProgrammer extends BaseRobotPlatesTileEntity {
+	public void triggerCompile() {
+		PlateCompiler compiler = new PlateCompiler();
+		compiler.compile(this, worldObj);
+	}
 }
