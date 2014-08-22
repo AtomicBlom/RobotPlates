32a86354278be2db6a471561a338133981c7835a
Steven Blom <atomicblom@binaryvibrance.net>
2014-08-22 19:15:23 +0930

    Clarifying DrawBlockHighlightEventHandler

--- src/main/java/net/binaryvibrance/robotplates/client/handler/DrawBlockHighlightEventHandler.java
+++ src/main/java/net/binaryvibrance/robotplates/client/handler/DrawBlockHighlightEventHandler.java
@@ -19,16 +19,21 @@ public class DrawBlockHighlightEventHandler {
 	@SubscribeEvent
 	public void onBlockHighlightEvent(DrawBlockHighlightEvent highlightEvent) {
 		MovingObjectPosition target = highlightEvent.target;
+		//We're only interested in blocks...
 		if (target.typeOfHit == MovingObjectPosition.MovingObjectType.BLOCK) {
 			World world = highlightEvent.player.worldObj;
 			TileEntity tileEntity = world.getTileEntity(target.blockX, target.blockY, target.blockZ);
+			//...From my mod
 			if (tileEntity instanceof BaseRobotPlatesTileEntity) {
+				//TODO: Recalculate the vector used to intersect the block so I can be more accurate with subblock detection.
 				BaseRobotPlatesTileEntity rpTileEntity = (BaseRobotPlatesTileEntity)tileEntity;
 				Vec3 targetBlockVec = Vec3.createVectorHelper(target.blockX, target.blockY, target.blockZ);
 				Vec3 vec = targetBlockVec.subtract(target.hitVec);
 				SubObject subObject = rpTileEntity.getSubObject(vec);
+				//... and we're hovering over one of my Sub-Objects.
 				if (subObject != null) {
 					subObject.RenderBoundingBox(highlightEvent.player, rpTileEntity, highlightEvent.partialTicks);
+					//Don't draw the block's highlight.
 					highlightEvent.setCanceled(true);
 				}
 			}
