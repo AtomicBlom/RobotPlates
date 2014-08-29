44a832d1f4f5f5afdce48e65f44083475abfc59c
Steven Blom <atomicblom@binaryvibrance.net>
2014-08-29 17:26:12 +0930

    Left over orientation code prevented compiling. Whoops.

--- src/main/java/net/binaryvibrance/robotplates/client/renderer/tileentity/BasePlateTileEntityRenderer.java
+++ src/main/java/net/binaryvibrance/robotplates/client/renderer/tileentity/BasePlateTileEntityRenderer.java
@@ -21,8 +21,8 @@ public abstract class BasePlateTileEntityRenderer<T extends BaseRobotPlatesTileE
 		T castTileEntity = (T) tileEntity;
 
 		GL11.glPushMatrix();
-		scaleTranslateRotate(x, y, z, castTileEntity.getOrientation());
-
+		GL11.glTranslated(x + 0.5, y, z + 0.5);
+		GL11.glScalef(model.scale, model.scale, model.scale);
 		renderInternal(castTileEntity);
 
 		GL11.glPopMatrix();
@@ -40,20 +40,4 @@ public abstract class BasePlateTileEntityRenderer<T extends BaseRobotPlatesTileE
 	}
 
 	protected abstract void render(T tileEntity);
-
-	private void scaleTranslateRotate(double x, double y, double z, ForgeDirection orientation) {
-		if (orientation == ForgeDirection.NORTH) {
-			GL11.glTranslated(x + 0.5, y, z + 0.5);
-		} else if (orientation == ForgeDirection.EAST) {
-			GL11.glTranslated(x + 0.5, y, z + 0.5);
-			GL11.glRotatef(-90F, 0F, 1F, 0F);
-		} else if (orientation == ForgeDirection.SOUTH) {
-			GL11.glTranslated(x + 0.5, y, z + 0.5);
-			GL11.glRotatef(180F, 0F, 1F, 0F);
-		} else if (orientation == ForgeDirection.WEST) {
-			GL11.glTranslated(x + 0.5, y, z + 0.5);
-			GL11.glRotatef(90F, 0F, 1F, 0F);
-		}
-		GL11.glScalef(model.scale, model.scale, model.scale);
-	}
 }
